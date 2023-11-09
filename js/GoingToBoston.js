15f9954c4f03186049a58e9a7a7dd3105050b250
Zidanni Clerigo <zclerigo40@stuy.edu>
2023-11-09 14:55:04 -0500

    js

--- js/GoingToBoston.js
+++ js/GoingToBoston.js
@@ -1,11 +1,12 @@
 "use strict"
 
-//const round = document.querySelector(".rounds")
 document.querySelector(".button_rounds").addEventListener("click", function(){
-    let list = document.querySelector(".button_rounds").classList;
-    if(list.contains("hidden")){
-        document.querySelector(".rounds").classList.remove("hidden");
-    }else{
-        document.querySelector(".rounds").classList.add("hidden");
-    } 
+    //let input_element_style = document.querySelector(".button_rounds").classList;
+    document.querySelector(".dice1").classList.add("hidden");
+    document.querySelector(".dice2").classList.add("hidden");
+    document.querySelector(".dice3").classList.add("hidden");
+    document.querySelector(".dice4").classList.add("hidden");
+    document.querySelector(".dice5").classList.add("hidden");
+    document.querySelector(".dice6").classList.add("hidden");
+    document.querySelector(".button_roll").classList.add("hidden");
 });
