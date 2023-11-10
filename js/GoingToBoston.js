8d93d2e731e3a154bacd091184cad1817c351152
Zidanni Clerigo <zclerigo40@stuy.edu>
2023-11-10 14:24:27 -0500

    js

--- js/GoingToBoston.js
+++ js/GoingToBoston.js
@@ -1,12 +1,19 @@
 "use strict"
 
+document.querySelector(".dice1").classList.add("hidden");
+document.querySelector(".dice2").classList.add("hidden");
+document.querySelector(".dice3").classList.add("hidden");
+document.querySelector(".dice4").classList.add("hidden");
+document.querySelector(".dice5").classList.add("hidden");
+document.querySelector(".dice6").classList.add("hidden");
+document.querySelector(".button_roll").classList.add("hidden");
+
 document.querySelector(".button_rounds").addEventListener("click", function(){
-    //let input_element_style = document.querySelector(".button_rounds").classList;
-    document.querySelector(".dice1").classList.add("hidden");
-    document.querySelector(".dice2").classList.add("hidden");
-    document.querySelector(".dice3").classList.add("hidden");
-    document.querySelector(".dice4").classList.add("hidden");
-    document.querySelector(".dice5").classList.add("hidden");
-    document.querySelector(".dice6").classList.add("hidden");
-    document.querySelector(".button_roll").classList.add("hidden");
+    document.querySelector(".dice1").classList.remove("hidden");
+    document.querySelector(".dice2").classList.remove("hidden");
+    document.querySelector(".dice3").classList.remove("hidden");
+    document.querySelector(".dice4").classList.remove("hidden");
+    document.querySelector(".dice5").classList.remove("hidden");
+    document.querySelector(".dice6").classList.remove("hidden");
+    document.querySelector(".button_roll").classList.remove("hidden");
 });
